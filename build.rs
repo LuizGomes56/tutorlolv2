fa86e0cd44efe114c3a60dc5b03b82e7ab233c2d
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-06-11 14:05:05 -0400

    Mudanças no gerador `writers`

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     env,
     fs::{self, DirEntry},
     path::Path,
@@ -7,33 +8,54 @@ use std::{
 fn main() {
     let out_path: String = env::var("OUT_DIR").unwrap();
     let writers_dir: &Path = Path::new("src/writers");
-    let mut match_arms: String = String::new();
+    {
+        let mut match_arms: String = String::new();
 
-    for entry in fs::read_dir(writers_dir).unwrap() {
-        let entry: DirEntry = entry.unwrap();
-        let name: String = entry.file_name().into_string().unwrap();
-        if name.ends_with(".rs") && name != "mod.rs" {
-            let content: String = fs::read_to_string(entry.path()).unwrap();
-            let formulas_path: &String = &format!("formulas/{}.rs", name.trim_end_matches(".rs"));
-            if !Path::new(formulas_path).exists() {
-                fs::File::create(formulas_path).unwrap();
+        for entry in fs::read_dir(writers_dir).unwrap() {
+            let entry: DirEntry = entry.unwrap();
+            let name: String = entry.file_name().into_string().unwrap();
+            if name.ends_with(".rs") && name != "mod.rs" {
+                let content: String = fs::read_to_string(entry.path()).unwrap();
+                let formulas_path: &String =
+                    &format!("formulas/{}.rs", name.trim_end_matches(".rs"));
+                if !Path::new(formulas_path).exists() {
+                    fs::File::create(formulas_path).unwrap();
+                }
+                fs::write(formulas_path, content.as_bytes()).unwrap();
+                let mod_name: &str = name.trim_end_matches(".rs");
+                match_arms += &format!(
+                    "\t\t\"{mod}\" => Some(writers::{mod}::transform(result)),\n",
+                    mod = mod_name
+                );
             }
-            fs::write(formulas_path, content.as_bytes()).unwrap();
-            let mod_name: &str = name.trim_end_matches(".rs");
-            match_arms += &format!(
-                "\t\t\"{mod}\" => Some(writers::{mod}::transform(result)),\n",
-                mod = mod_name
-            );
         }
+
+        let writer_transform_code: String = format!(
+            "pub fn try_transform(name: &str, result: CdnChampion) -> Option<Champion> {{\n\tmatch name {{\n{match_arms}\t\t_ => None,\n\t}}\n}}"
+        );
+
+        fs::write(
+            format!("{}/writers_generated.rs", out_path),
+            writer_transform_code,
+        )
+        .unwrap();
     }
 
-    let writer_transform_code: String = format!(
-        "pub fn try_transform(name: &str, result: CdnChampion) -> Option<Champion> {{\n\tmatch name {{\n{match_arms}\t\t_ => None,\n\t}}\n}}"
-    );
-
-    fs::write(
-        format!("{}/writers_generated.rs", out_path),
-        writer_transform_code,
-    )
-    .unwrap();
+    {
+        if let Ok(champion_names) = fs::read_to_string("internal/champion_names.json") {
+            if let Ok(map) = serde_json::from_str::<HashMap<String, String>>(&champion_names) {
+                for (_, champion_id) in map.into_iter() {
+                    let id = champion_id.to_lowercase();
+                    if !Path::new(&writers_dir.join(format!("{}.rs", id))).exists() {
+                        let content = r#"use super::{Ability, CdnChampion, Champion,
+	                        HashMap, Target, extract_ability_damage}; 
+                            // #![auto_generated] 
+                            #[writer_macros::writer] pub fn transform(data: CdnChampion) -> Champion {}"#;
+                        fs::write(writers_dir.join(format!("{}.rs", id)), content.as_bytes())
+                            .unwrap();
+                    }
+                }
+            }
+        };
+    }
 }
