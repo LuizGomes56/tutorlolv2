e1f17ff1bd330d6ca4f4206fedbc7ebe8ab79959
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-08-14 12:39:28 -0400

    internal_comptime constants mod.rs

---
+++ internal_comptime/src/data/mod.rs
@@ -0,0 +1,13 @@
+pub mod champions;
+pub mod items;
+pub mod runes;
+
+pub(crate) use crate::cache::*;
+pub(crate) use bincode::{Decode, Encode};
+pub(crate) use shared_types::*;
+#[rustfmt::skip]
+pub use champions::*;
+#[rustfmt::skip]
+pub use items::*;
+#[rustfmt::skip]
+pub use runes::*;
