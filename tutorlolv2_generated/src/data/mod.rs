853eb9c796eafc1dcb6824458d164117889d9d28
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-09-19 23:58:34 -0400

    Image convertion crate file split. Spritesheets may be removed in the future.

--- tutorlolv2_generated/src/data/mod.rs
+++ tutorlolv2_generated/src/data/mod.rs
@@ -4,10 +4,7 @@ pub mod runes;
 
 pub(crate) use crate::cache::*;
 pub(crate) use bincode::{Decode, Encode};
-pub(crate) use tutorlolv2_shared::*;
-#[rustfmt::skip]
 pub use champions::*;
-#[rustfmt::skip]
 pub use items::*;
-#[rustfmt::skip]
 pub use runes::*;
+pub(crate) use tutorlolv2_shared::*;
