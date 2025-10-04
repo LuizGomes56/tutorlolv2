39499b0262298a2ebf7c79b6e2c27095aa26696e
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-10-03 21:24:01 -0400

    Darius, Fiora, Garen, Gnar

--- tutorlolv2_dev/src/generators/garen.rs
+++ tutorlolv2_dev/src/generators/garen.rs
@@ -1,11 +1,12 @@
 use super::*;
 
-// #![preserve]
+// #![preserve] "15.19.1" | "10/03/2025"
 
 #[tutorlolv2_macros::generator]
 pub fn gen_garen(data: CdnChampion) -> Champion {
-    ability!(q, (1, 0, _1, Min));
-    ability!(w, (0, 0, _1Min, Min));
-    ability!(e, (0, 0, _1Min, Min), (3, 0, _2Max, Max));
-    ability!(r, (0, 0, _1Min, Min));
+    ability!(q, (1, 0, Void, Min));
+    ability!(w, (0, 0, Void, Min));
+    ability!(e, (0, 0, Min, Min), (3, 0, Max, Max));
+    ability!(r, (0, 0, Void, Min));
+    merge_ability!(E::Min, E::Max);
 }
--- tutorlolv2_dev/src/generators/gnar.rs
+++ tutorlolv2_dev/src/generators/gnar.rs
@@ -1,11 +1,13 @@
 use super::*;
 
-// #![preserve]
+// #![preserve] "15.19.1" | "10/03/2025"
 
 #[tutorlolv2_macros::generator]
 pub fn gen_gnar(data: CdnChampion) -> Champion {
-    ability!(q, (0, 0, _1Min, Min), (0, 1, _2Min, Min), (0, 0, Mega, Min));
-    ability!(w, (2, 0, _1, Min), (0, 0, _2Min, Min));
-    ability!(e, (4, 0, _1Min, Min), (0, 0, _2Min, Min));
-    ability!(r, (0, 0, _1Max, Max), (1, 1, _2Min, Min));
+    ability!(q, (0, 0, Min, Min), (0, 1, Max, Max), (0, 0, Mega, Min));
+    ability!(w, (2, 0, Void, Min), (0, 0, Mega, Min));
+    ability!(e, (4, 0, Void, Min), (0, 0, Mega, Min));
+    ability!(r, (0, 0, Max, Max), (1, 1, Min, Min));
+    merge_ability!(Q::Min, Q::Max);
+    merge_ability!(R::Min, R::Max);
 }
