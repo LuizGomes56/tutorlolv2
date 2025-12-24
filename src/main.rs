c4c327a6c9319e49f7179f2c22aa67e17995ae86
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-12-24 16:37:40 -0300

    Image folder published

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use tutorlolv2::{model::InputGame, riot::RiotRealtime};
 fn main() {
     unsafe {
         tutorlolv2::calculator(core::mem::transmute([0u8; { size_of::<InputGame>() }]));
-        tutorlolv2::realtime(&*(0 as *const () as *const RiotRealtime) as &RiotRealtime<'_>);
+        tutorlolv2::realtime(&*(0 as *const () as *const RiotRealtime) as &_);
     }
 }
