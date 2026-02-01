3dd3f69bcf2105421346c5a7f1d2c763b5aa6609
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-02-01 02:26:08 -0500

    Kalista black spear split

--- tutorlolv2_dev/src/generators/gen_items/kalistas_black_spear_3599.rs
+++ tutorlolv2_dev/src/generators/gen_items/kalistas_black_spear_3599.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-impl Generator<ItemData> for KalistasBlackSpear {
+impl Generator<ItemData> for KalistasBlackSpear3599 {
     fn generate(self: Box<Self>) -> MayFail<ItemData> {
         /* No implementation */
         self.end()
