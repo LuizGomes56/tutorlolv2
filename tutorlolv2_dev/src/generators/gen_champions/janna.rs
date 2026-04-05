b29e71fda28bfc72c7cbf56f6a12aae582b9adb1
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-04-04 21:27:22 -0400

    Jinx, Janna

--- tutorlolv2_dev/src/generators/gen_champions/janna.rs
+++ tutorlolv2_dev/src/generators/gen_champions/janna.rs
@@ -1,10 +1,15 @@
 use super::*;
 
+// #![stable]
+
 impl Generator<Champion> for Janna {
     fn generate(mut self: Box<Self>) -> MayFail<Champion> {
-        self.ability(Key::Q, [(0, 0, _1), (0, 1, _2), (0, 2, _3)]);
-        self.ability(Key::W, [(0, 0, _1)]);
-        self.ability(Key::E, [(2, 0, _1)]);
+        self.ability(Key::Q, [(0, 1, Max), (0, 2, Min)]);
+        self.ability(Key::W, [(0, 0, Void)]);
+
+        self.attr(Area, [Q(Min), Q(Max)])?;
+        self.combo([Ability(W(Void)), Attack, Ability(Q(Max)), Attack])?;
+
         self.end()
     }
 }
--- tutorlolv2_dev/src/generators/gen_champions/jinx.rs
+++ tutorlolv2_dev/src/generators/gen_champions/jinx.rs
@@ -1,10 +1,44 @@
 use super::*;
 
+// #![stable]
+
 impl Generator<Champion> for Jinx {
     fn generate(mut self: Box<Self>) -> MayFail<Champion> {
-        self.ability(Key::W, [(0, 0, _1)]);
-        self.ability(Key::E, [(0, 0, _1)]);
-        self.ability(Key::R, [(1, 0, _1), (1, 1, _2), (2, 0, _3), (2, 1, _4)]);
+        self.ability(Key::W, [(0, 0, Void)]);
+        self.ability(Key::E, [(0, 0, Void)]);
+        self.ability(
+            Key::R,
+            [
+                // Surroundings
+                (1, 0, _1Max),
+                (1, 1, _1Min),
+                // Primary Target
+                (2, 0, Max),
+                (2, 1, Min),
+            ],
+        );
+
+        let q = self
+            .data
+            .abilities
+            .q
+            .first()
+            .ok_or("Failed to find Jinx's Q")?
+            // n = 1 generates unrecognized pattern
+            .format(vec![format!("1.1 * {AttackDamage}"); 5]);
+
+        self.insert(Q(Void), q);
+
+        self.attr(AreaOnhitMax, [Q(Void)])?;
+        self.attr(Area, [Q(Void), E(Void), R(_1Max), R(_1Min), R(Max), R(Min)])?;
+        self.combo([
+            Ability(E(Void)),
+            Attack,
+            Ability(R(Min)),
+            Attack,
+            Ability(W(Void)),
+        ])?;
+
         self.end()
     }
 }
