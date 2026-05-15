f8786caaa61d4b79c05cbfc4fc91bd26d02a59b4
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-05-14 23:28:06 -0400

    Diana

--- tutorlolv2_dev/src/generators/gen_champions/diana.rs
+++ tutorlolv2_dev/src/generators/gen_champions/diana.rs
@@ -1,33 +1,25 @@
 use super::*;
 
 impl Generator for Diana {
-    #[warn(unstable_features)]
     fn generate(&mut self) -> MayFail {
-        self.ability(
-            Key::P,
-            [
-                (1, _1), /* Innate - Moonsilver Blade */
-                (2, _2), /* Innate - Moonsilver Blade [1] */
-                (3, _3), /* Innate - Moonsilver Blade [2] */
-            ],
-        )
-        .ability(Key::Q, [(0, _1) /* Magic Damage */])
-        .ability(
-            Key::W,
-            [
-                (0, _1), /* Magic Damage per Orb */
-                (3, _2), /* Total Magic Damage */
-            ],
-        )
-        .ability(Key::E, [(0, _1) /* Magic Damage */])
-        .ability(
-            Key::R,
-            [
-                (0, _1), /* Bonus Damage Per Champion */
-                (1, _2), /* Magic Damage */
-                (3, _3), /* Total Damage Vs. 5 Champions */
-            ],
-        )
-        .end()
+        self.ability(Key::P, [(1, Void) /* Innate - Moonsilver Blade */])
+            .ability(Key::Q, [(0, _1) /* Magic Damage */])
+            .ability(
+                Key::W,
+                [
+                    (0, Min), /* Magic Damage per Orb */
+                    (3, Max), /* Total Magic Damage */
+                ],
+            )
+            .ability(Key::E, [(0, Void) /* Magic Damage */])
+            .ability(
+                Key::R,
+                [
+                    (0, _1),  /* Bonus Damage Per Champion */
+                    (1, Min), /* Magic Damage */
+                    (3, Max), /* Total Damage Vs. 5 Champions */
+                ],
+            )
+            .end()
     }
 }
