22b4e6fa38971301d4fc93eb304425bee6c1bef7
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-07-03 22:27:43 -0400

    Tristana

--- build/src/generators/impls/champions/tristana.rs
+++ build/src/generators/impls/champions/tristana.rs
@@ -1,19 +1,19 @@
 use super::*;
 
 impl Generator for Tristana {
-    #[warn(unstable_features)]
     fn generate(&mut self) -> MayFail {
         self.ability(Key::W, [(0, Void) /* Magic Damage */])
             .ability(
                 Key::E,
                 [
-                    (0, _1), /* Bonus Damage Per Stack */
-                    (1, _2), /* Full Stack Bonus Damage */
-                    (2, _3), /* Full Stack Physical Damage */
-                    (3, _4), /* Magic Damage */
-                    (4, _5), /* Minimum Physical Damage */
+                    // (0, _1), /* Bonus Damage Per Stack */
+                    // (1, _2), /* Full Stack Bonus Damage */
+                    (2, Max),  /* Full Stack Physical Damage */
+                    (3, Void), /* Magic Damage */
+                    (4, Min),  /* Minimum Physical Damage */
                 ],
             )
+            .comment(E(Void), "Passive explosion damage on kill")?
             .ability(Key::R, [(1, Void) /* Magic Damage */])
             .end()
     }
